ea94b163cb54b0eb8d040f3fc75b4a5df79542de
Timo Deuschle <timo.deuschle@kaiserkraft-europa.de>
2018-11-23 17:02:43 +0100

    (feat) WIP join room

--- server/server.js
+++ server/server.js
@@ -9,7 +9,10 @@ io.listen(PORT);
 console.log('Socket.io listening on port ', PORT);
 
 io.on(CONNECTION, socket => {
-  socket.on(REQUEST_NEW_ROOM, () => {
-    socket.emit(NEW_ROOM_CREATED, 'foobar')
+  socket.on(REQUEST_NEW_ROOM, event => {
+    // TODO verify that the room joining actually works as intended
+    const room = 'foobar'
+    socket.join(room)
+    socket.emit(NEW_ROOM_CREATED, room)
   });
 });
